53a789e4ca528c6b0698c19e84acf9b9e83d4bd5
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-05-12 23:24:49 +0530

    my-finance-trackercomplete project

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,8 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
-import React from "react";
 
-const Page = () => {
-    return <SignUp />
+export default function Page() {
+  return <SignUp />;
 }
-
-export default Page
